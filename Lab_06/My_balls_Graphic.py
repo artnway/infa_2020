cb3712c539227ac940b029c99e7fb32046997d5a
artnway <lukmor@ya.ru>
2021-12-21 02:58:20 +0300

    Added round counter and bonus points for shots. Added endgame with finalscore print

--- Lab_06/My_balls_Graphic.py
+++ Lab_06/My_balls_Graphic.py
@@ -32,8 +32,8 @@ def make_pool():
 
 class Ball:
     def __init__(self, x=0, y=0, r=0, color=None, vx=0, vy=0, eye=0, eye_rect=0, items=0):
-        x = randint(75, 750)
-        y = randint(75, 750)
+        x = randint(75, 700)
+        y = randint(75, 700)
         r = 25
         color = "Black"
         vx = randint(-2, 2)
@@ -74,17 +74,27 @@ class Ball:
 class ScoreCounter:
     def __init__(self):
         score = 0
+        level = 1
         self.score = score
+        self.level = level
 
     def add_score(self):
         self.score = self.score + 5
 
+    def add_level(self):
+        self.level = self.level + 1
+
     def show_score(self):
         return self.score
 
+    def show_level(self):
+        return self.level
+
+    def draw_level(self):
+        return "Level: " + str(self.level)
+
     def draw_score(self):
         return "Score: " + str(self.score)
-
 
 class Counter:
     def __init__(self):
@@ -108,6 +118,8 @@ class Counter:
     def show_shots(self):
         return self.shots
 
+
+#FIXIT Где-то тут бага, шарики улетают за пределы поля в верхнем левом углу, ВРОДЕ.
 def draw_objects():
     rect(screen, "black", (36, 61, 775, 775), 3)
     clock.tick(FPS)
@@ -118,6 +130,7 @@ def draw_objects():
             b.reverse_x()
         if b.y > 800 > b.x or b.y < 75 < b.x:
             b.reverse_y()
+
 
 
 class Screen_text_drawer:
@@ -135,20 +148,26 @@ class Screen_text_drawer:
         self.textShotsRect = self.textShots.get_rect()
         self.textShotsRect.center = (700, 25)
 
-    def screen_round(self):
-        self.textSurfaceObj = self.fontObj.render(s.draw_round(), True, "black")
-        self.textRectObj = self.textRectObj.get_rect()
-        self.textRectObj.center = (110, 25)
+    def screen_level(self):
+        self.textLevel = self.fontObj.render(ss.draw_level(), True, "black")
+        self.textLevelRect = self.textLevel.get_rect()
+        self.textLevelRect.center = (350, 25)
 
     def screen_game_over(self):
-        self.textSurfaceObj = self.fontObj.render("Game Over", True, "black")
-        self.textRectObj = self.textSurfaceObj.get_rect()
-        self.textRectObj.center = (110, 25)
+        self.textGameover = self.fontObj.render("Game Over", True, "black")
+        self.textGemeoverRect = self.textGameover.get_rect()
+        self.textGemeoverRect.center = (110, 25)
+
+    def screen_double_kill(self):
+        self.textDoublekill = self.fontObj.render("Double Kill!", True, "black")
+        self.textDoublekillRect = self.textDoublekill.get_rect()
+        self.textDoublekillRect.center = (110, 25)
 
 
-    def screen_draw_text(self):
+    def screen_draw_counters(self):
         screen.blit(self.textScore, self.textScoreRect)
         screen.blit(self.textShots, self.textShotsRect)
+        screen.blit(self.textLevel, self.textLevelRect)
 
 def game_loop():
     finished = False
@@ -156,7 +175,8 @@ def game_loop():
         ssr = Screen_text_drawer()
         ssr.screen_score()
         ssr.screen_shots()
-        ssr.screen_draw_text()
+        ssr.screen_level()
+        ssr.screen_draw_counters()
         draw_objects()
         for event in pygame.event.get():
             for b in Pool:
@@ -169,14 +189,11 @@ def game_loop():
                         b.dead()
                         s.dead()
                         ss.add_score()
-                        print('В точку!')
                         print(s.allive)
             if event.type == pygame.QUIT or s.shots < 1:
                 finished = True
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 s.shot()
-                print("Осталось ", s.show_shots(), "выстрелов")
-
         pygame.display.update()
         screen.fill("white")
         back = pygame.image.load("maxresdefault_2.jpg").convert_alpha()
@@ -190,21 +207,19 @@ while gameround is False:
     s = Counter()
     Pool = make_pool()
     game_loop()
-"""    if s.allive < 1:
+    ss.add_level()
+    for i in range(s.show_shots()):
+        ss.score += 1
+    if s.allive < 1:
         print("Новый раунд")
         gameround = False
     else:
         gameround = True
-"""
+print("Вы набрли:", ss.show_score(), "очков!")
+pygame.quit()
 
-"""Сделать раунды: старт, гейм овер. Сделать отображение кол-ва выстрелов, кол-ва очков,
-и прикрутить прицел на мышку."""
 
-"""pygame.quit()
 
-if s.shots > 0:
-    for i in range(s.shots):
-        s.add_point()
-    print("Вы набрли:", s.show_score(), "очков!")
-else:
-    print("Вы набрли:", s.show_score(), "очков!")"""
+"""Доделать раунды: старт, гейм овер. Сделать дабл и трипл килл, мб бонусный раунд с шеренганами."""
+
+
